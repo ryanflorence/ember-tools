ff30f4846ff59539a7e83061f9c31543c46691f7
Karl Guillotte <karl.guillotte.1@gmail.com>
2013-02-22 20:39:18 -0800

    Add Mixin generator.

--- src/generators/index.js
+++ src/generators/index.js
@@ -4,4 +4,5 @@ exports.controller = require('./controller');
 exports.template = require('./template');
 exports.route = require('./route');
 exports.scaffold = require('./scaffold');
+exports.mixin = require('./mixin');
 
