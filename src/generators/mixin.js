07f4b412c735a521c8815d9fff3c1dba97003803
Ryan Florence <rpflorence@gmail.com>
2013-05-30 22:46:00 -0600

    mixin prompt

--- src/generators/mixin.js
+++ src/generators/mixin.js
@@ -1,10 +1,17 @@
 var fs = require('../util/fs');
 var inflector = require('../util/inflector');
+var msg = require('../util/message');
 
 module.exports = function(resource) {
   var resourceName = inflector.underscore(inflector.singularize(resource));
   fs.writeGenerator('mixin', resourceName, {
-    objectName: inflector.objectify(resourceName)
+    objectName: inflector.objectify(resourceName),
+    mixinType: promptMixinType()
   });
 };
+
+function promptMixinType() {
+  var isControllerMixin = msg.confirm('Is this a controller mixin?');
+  return isControllerMixin ? 'Controller' : '';
+}
 
